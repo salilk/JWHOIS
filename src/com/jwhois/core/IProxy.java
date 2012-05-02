f1f75c09860c5a603685ca7c9883081bb6b2c51f
salilk <salil.kalia@gmail.com>
2012-05-02 11:36:35 +0530

    directory structure modified: transformed to maven project

--- src/com/jwhois/core/IProxy.java
+++
@@ -1,5 +0,0 @@
-package com.jwhois.core;
-
-public interface IProxy {
-	String getProxy();
-}
--- src/com/jwhois/core/Logger.java
+++
@@ -1,9 +0,0 @@
-package com.jwhois.core;
-
-public interface Logger {
-	public enum LEVEL {
-		ERROR, WARN, INFO, DEBUG
-	};
-
-	void doLog(LEVEL lv, String info, Exception e);
-}
