44516488ac279e1942d479633dc680d1841a8bce
salilk <salil.kalia@gmail.com>
2012-05-03 15:23:13 +0530

    godaddy case fixed.

--- src/test/java/com/jwhois/core/WhoisClientTest.java
+++ src/test/java/com/jwhois/core/WhoisClientTest.java
@@ -2,6 +2,10 @@ package com.jwhois.core;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by IntelliJ IDEA.
  * User: salil
@@ -18,10 +22,21 @@ public class WhoisClientTest extends TestCase {
         super.tearDown();
     }
 
+    //TODO: need to mock the responses (hitting internet)
     public void testDomLookup() throws Exception {
-        WhoisEngine whoisEngine = new WhoisEngine("fullcontact.com");
-        WhoisMap map = whoisEngine.build();
-        System.out.println(map.getMap());
+        //String[] names = new String[]{"finddesire.com", "fullcontact.com", "yahoo.com", "google.com"};
+        String[] names = new String[]{"finddesire.com"};
+        for (String name : names) {
+            System.out.println("============ " + name + " ====================");
+            WhoisEngine whoisEngine = new WhoisEngine(name);
+            WhoisMap whoisMap = whoisEngine.build();
+            if ((whoisMap != null) && (whoisMap.getMap() != null))
+                for (String key : whoisMap.getMap().keySet()) {
+                    System.out.println(key + " : " + whoisMap.get(key));
+                    System.out.println("-------------------------");
+                }
+            System.out.println("================================\n\n");
+        }
     }
 
 }
