a66cfdca380965032430da687cae406af896f355
salilk <salil.kalia@gmail.com>
2012-06-11 16:53:32 +0530

    unit test added for processing 1st level whois (Socket) Response

--- src/test/java/com/jwhois/core/WhoisEngineTest.java
+++ src/test/java/com/jwhois/core/WhoisEngineTest.java
@@ -2,6 +2,10 @@ package com.jwhois.core;
 
 import junit.framework.TestCase;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by IntelliJ IDEA.
  * User: salil
@@ -19,8 +23,41 @@ public class WhoisEngineTest extends TestCase {
         super.tearDown();
     }
 
-    public void testSomething() throws Exception {
-
+    public void testBuild_DEEP() throws Exception {
+//        WhoisEngine engine = new WhoisEngine("fullcontact.com", false);
+//        engine.build();
+//        engine.setLineFilter("com.whois-servers.net");
+        //System.out.println("in LINE FILTER: ");
     }
+
+    public void testLineFilter() throws Exception{
+        WhoisEngine engine = new WhoisEngine("fullcontact.com", false);
+        engine.setLineFilter("com.whois-servers.net");
+        List<String> responseLines = getMockResponseLines("fullcontact.com");
+        engine.processSocketResponse(responseLines);
+        assertEquals(13, responseLines.size());
+        assertTrue(responseLines.contains("Domain Name: FULLCONTACT.COM"));
+        assertTrue(responseLines.contains("Creation Date: 06-jun-2000"));
+        assertTrue(responseLines.contains("Expiration Date: 06-jun-2013"));
+        assertTrue(responseLines.contains("Updated Date: 05-oct-2011"));
+        assertTrue(responseLines.contains("Name Server: DNS1.NETTICA.COM"));
+        assertTrue(responseLines.contains("Whois Server: whois.name.com"));
+        assertTrue(responseLines.contains("Registrar: NAME.COM LLC"));
+    }
+
+    private List<String> getMockResponseLines(String domain) throws IOException {
+        List<String> responseLines = new ArrayList<String>();
+        File file = new File("src/test/data/level1/"+domain+".txt");
+        FileInputStream fis = new FileInputStream(file);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        String line;
+        while ((line = br.readLine()) != null) {
+            responseLines.add(line.trim());
+        }
+        fis.close();
+
+        return responseLines;
+    }
+
 
 }
